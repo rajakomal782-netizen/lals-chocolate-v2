cfa1acf8fbf11aa05e5749250b7f58117ffddaa8
Komal Jethwa <rajakomal782@gmail.com>
2026-04-01 10:22:37 -0700

    komal ka bhand

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-
- const slides = document.querySelectorAll(".slide");
+const slides = document.querySelectorAll(".slide");
 let currentIndex = 0;
 
 // Show the first slide initially
-slides[currentIndex].classList.add("active ");
+slides[currentIndex].classList.add("active");
 
 function showSlide(index) {
-    slides.forEach(slide => slide.classList.remove("active "));
-    slides[index].classList.add("active ");
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[index].classList.add("active");
 }
 
 function nextSlide() {
@@ -17,3 +16,33 @@ function nextSlide() {
 
 // Change slide every 3 seconds
 setInterval(nextSlide, 3000);
+
+
+
+
+$('.owl-carousel').owlCarousel({
+    rtl: true,
+    loop: true,
+    margin: 15,
+    nav: true,
+    dots: true,   // 👈 yeh ON karo
+    slideBy: 3,
+    responsive: {
+        0: { items: 1 },
+        600: { items: 2 },
+        1000: { items: 3 }
+    }
+});
+
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    items: 3
+});
+
+
+
+
+
